fcb8fd09530e4af3bf3ae355a66045e2c1d216e2
Sunwoo Jeong <seraphina818@gmail.com>
2019-11-21 18:30:16 +0900

    Update veridicality-pilot1.js

--- experiment-pilot/veridicality-pilot1.js
+++ experiment-pilot/veridicality-pilot1.js
@@ -1,6 +1,4 @@
 //(if collecting data with php/server instead of MTurk)
-// Do not use below
-// var submitAddress = "http://hosting02.snu.ac.kr/~sunwooj/cgi-bin/process.php";
 
 var experimentName = "veridicality-pilot";
 var submitAddress = "https://web.stanford.edu/~junhongc/cgi-bin/process.php";
